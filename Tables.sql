4a4b9cb5adc160f8ca4af64c051ef58b438679c9
Anju Maurya <66139961+AnjuMau8418@users.noreply.github.com>
2024-12-07 12:32:18 +0530

    Add files via upload

---
+++ Tables.sql
@@ -0,0 +1,20 @@
+/*show the constituencywise_details table*/
+SELECT *
+FROM constituencywise_details
+
+/*show the constituencywise_results table*/
+SELECT *
+FROM constituencywise_results
+
+/*show the partywise_results table*/
+SELECT *
+FROM partywise_results
+
+/*show the statewise_results table*/
+SELECT *
+FROM statewise_results
+
+/*show the states table*/
+SELECT *
+FROM states
+
